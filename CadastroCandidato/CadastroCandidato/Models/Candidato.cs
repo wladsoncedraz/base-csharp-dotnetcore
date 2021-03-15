08025a35796ae090ff1caed5f5e37339780f25ff
wladsoncedraz <wladylinux@gmail.com>
2021-03-15 19:24:55 -0300

    Iniciando o projeto CRUD em C#

---
+++ CadastroCandidato/CadastroCandidato/Models/Candidato.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CadastroCandidato.Models
+{
+    [Table("Candidato")]
+    public class Candidato
+    {
+        [Key]
+        public int CandidatoID { get; set; }
+
+        [Required]
+        public string vchNome { get; set; }
+
+        [Required]
+        public string vchEmail { get; set; }
+
+        [Required]
+        public string vchCidade { get; set; }
+
+        [Required]
+        public string vchEstado { get; set; }
+    }
+}
---
+++ CadastroCandidato/CadastroCandidato/Models/ICandidatoDAL.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace CadastroCandidato.Models
+{
+    interface ICandidatoDAL
+    {
+        IEnumerable<Candidato> GetAllCandidatos();
+        void AddCandidato(Candidato candidato);
+        void UpdateCandidato(Candidato candidato);
+        Candidato GetCandidato(int? CandidatoID);
+        void DeleteCandidato(int? CandidatoID);
+    }
+}
